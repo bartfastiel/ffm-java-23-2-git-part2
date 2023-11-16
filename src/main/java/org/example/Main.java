69ad5787b6f6975678e8d9c0234a3213b19b1217
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-16 10:16:11 +0100

    Add nice welcome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Timeline");
+        System.out.println("Welcome at timeline");
         System.out.println("Timeline advanced");
         System.out.println("Timeline super");
 
