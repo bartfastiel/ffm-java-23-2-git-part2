ce48863ff6d1c2d2edb43d45a26a4ca197d2a3ca
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-16 09:53:42 +0100

    Add timeline

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Timeline");
+
         System.out.println("Hello world!");
     }
 }
