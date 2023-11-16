70f44bf4081e85d8b182677f506fd53a21a0a0a5
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-16 09:55:15 +0100

    Improve timeline

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Timeline");
+        System.out.println("Timeline advanced");
 
         System.out.println("Hello world!");
     }
